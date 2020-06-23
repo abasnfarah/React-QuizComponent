e06bdebfdc4f7e6a78bbb04fffc33f135d90a201
AbasFarah <Abas.Farah@target.com>
2020-06-23 11:45:20 -0500

    Fixing QuizEnd component logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
   }
 
   render() {
-    const isQuizEnd = true;
+    const isQuizEnd = false;
       //this.state.quiz_position - 1 === quizData.quiz_questions.length;
     console.log(isQuizEnd);
     return (
